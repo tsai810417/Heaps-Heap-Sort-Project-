c399a7f2646bebdd178c569d334e4fd38fffb7bf
tsai810417 <tsai_cheng@yahoo.com>
2018-05-03 14:44:22 -0700

    solve finding median problem with Heap

--- lib/k_largest_elements.rb
+++ lib/k_largest_elements.rb
@@ -17,3 +17,58 @@ def k_largest_elements(array, k)
 
   res
 end
+
+class EasyMedian
+  attr_accessor :min_heap, :max_heap
+  def initialize
+    prc = Proc.new{ |a,b| b <=> a }
+    @length = 0
+    @min_heap = BinaryMinHeap.new()
+    @max_heap = BinaryMinHeap.new(&prc)
+  end
+
+  def add(val)
+    if @length == 0
+      @min_heap.push(val)
+    elsif val <= @min_heap.peek
+      @max_heap.push(val)
+    else
+      @min_heap.push(val)
+    end
+    @length += 1
+    EasyMedian.check(@max_heap, @min_heap)
+    p @max_heap
+    p @min_heap
+  end
+
+  def find_median
+    if @length.even?
+      (@max_heap.peek + @min_heap.peek) / 2
+    elsif @max_heap.length > @min_heap.length
+       @max_heap.peek
+    else
+      @min_heap.peek
+    end
+  end
+
+  private
+
+  def self.check(max_heap, min_heap)
+    if min_heap.length == max_heap.length + 2
+      max_heap.push(min_heap.extract)
+    elsif max_heap.length == min_heap.length + 2
+      min_heap.push(max_heap.extract)
+    end
+  end
+end
+
+test = EasyMedian.new
+test.add(8)
+p "-----------"
+p "I am median #{test.find_median()}"
+test.add(16)
+p "-----------"
+p "I am median #{test.find_median()}"
+test.add(4)
+p "-----------"
+p "I am median #{test.find_median()}"
